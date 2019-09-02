bae36f6cdab5529a1b9b8c5b6b54f0c8a79c8f1b
domhuff <dominikhuffield@u.boisestate.edu>
2019-09-02 00:14:42 -0600

    Made so file would return a dictionary

--- getPlaylists.py
+++ getPlaylists.py
@@ -1,26 +1,36 @@
 import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
 
-client_credentials_manager = SpotifyClientCredentials()
+client_id = '5572530ac0664df2b34373f3f41db89b'
+client_secret = '15734ca0cac44e3c8ee4cc238bc895cf'
+uri = 'spotify:user:22okarqslvrgo5yvlkkfvr64a:playlist:2q1iIfQJgOkHumhCQg4Hrg'
+
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# user = '22okarqslvrgo5yvlkkfvr64a?'
 
 # if len(sys.argv) > 1:
 #     user = sys.argv[1]
 
-uri = 'spotify:user:22okarqslvrgo5yvlkkfvr64a:playlist:2q1iIfQJgOkHumhCQg4Hrg'
+
+
 username = uri.split(':')[2]
 playlist_id = uri.split(':')[4]
 
 results = sp.user_playlist(username, playlist_id)
-# print(results['tracks']['items'])
-for track in results['tracks']['items']:
+results = results['tracks']['items']
+# print(results[0].keys())
+playlist = {}
+for track in results:
     art = list()
     for artist in track['track']['artists']:
         art.append(artist['name'])
-    print(track['track']['name'] + '   ***   ' + str(art))
+    playlist.update({track['track']['name'] : art})
+print(playlist)
+
+
 # trackz = results['tracks']['items'][0]['track']
 # print (results['tracks'])
 # for track in results['tracks']['items'][0]['track']:
