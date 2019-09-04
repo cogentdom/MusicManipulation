81748bfd6c09271d8296b70b9a0a52fe3e450aa5
domhuff <dominikhuffield@u.boisestate.edu>
2019-09-04 01:28:00 -0600

    audio analysis method

--- getPlaylists.py
+++ getPlaylists.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 client_id = '5572530ac0664df2b34373f3f41db89b'
 client_secret = '15734ca0cac44e3c8ee4cc238bc895cf'
-uri = 'spotify:user:22okarqslvrgo5yvlkkfvr64a:playlist:2q1iIfQJgOkHumhCQg4Hrg'
+uri = 'spotify:user:22okarqslvrgo5yvlkkfvr64a:playlist:37i9dQZF1DXdgz8ZB7c2CP'
+# uri = 'spotify:user:22okarqslvrgo5yvlkkfvr64a:playlist:2q1iIfQJgOkHumhCQg4Hrg'
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -32,16 +33,17 @@ for track in results:
     playlist_art.update({track['track']['name'] : art})
     playlist_uri.update({track['track']['name'] : track['track']['uri']})
     uri_list.append(track['track']['uri'])
-
+    print(track['track']['name'
+                         ''], art)
 # Audio analysis Doc
 # https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-analysis/#rhythm
-analyze = sp.audio_analysis(playlist_uri['Loyal']) # dict_keys(['meta', 'track', 'bars', 'beats', 'tatums', 'sections', 'segments'])
-analyze = analyze['bars']
+# analyze = sp.audio_analysis(playlist_uri['Loyal']) # dict_keys(['meta', 'track', 'bars', 'beats', 'tatums', 'sections', 'segments'])
+# analyze = analyze['bars']
 # print(analyze)
 # print(analyze.keys())
 
 
 # Audio Features
 # https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/
-analy = sp.audio_features(uri_list[0])
-print(analy)
+# analy = sp.audio_features(uri_list[0])
+# print(analy)
